3a379ec78a1b140ed91ed5717910ac48575ead0a
ARealSeal <53285975+arealseal@users.noreply.github.com>
2021-11-02 08:52:03 -0700

    fixed the prompt a bit

--- thing.py
+++ thing.py
@@ -1,5 +1,5 @@
 #init shit <- bruh what's this mean
-username = input("Idk how to find this on my own so what's your name lamo")
+username = input("Idk how to find this on my own so what's your name lamo? >")
 path = "C:\\Users\\"+username+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
 # parse the thing and stuff
 # spotify_version = 
