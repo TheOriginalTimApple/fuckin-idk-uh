2b720b5c43f7ebd9490837a8fbc762936fc20e54
TheOriginalTimApple <66660173+TheOriginalTimApple@users.noreply.github.com>
2021-11-02 15:54:50 -0700

    Made some changes to code structure

--- thing.py
+++ thing.py
@@ -1,10 +1,13 @@
+#establishes libraries req'd
 import subprocess 
-import getpass
+import getpass as gp
 
-# @TheOriginalTimApple, what's this do?
-def run(self, cmd):
-    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
-    return completed
-
-path = "C:\\Users\\"+getpass.getuser()+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
+#establishes filepath req'd to obtain Spotify version info
+path = "C:\\Users\\"+gp.getuser()+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
 print(path)
+
+
+# @TheOriginalTimApple, what's this do? >TheOriginalTimApple: Not sure, saw it in a webpage about PS integration with python
+#def run(self, cmd):
+   # completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+   # return completed
