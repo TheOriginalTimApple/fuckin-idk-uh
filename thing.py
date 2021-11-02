45c6701e5d94a58a5e425b6fbddaa0055bd29259
TheOriginalTimApple <66660173+TheOriginalTimApple@users.noreply.github.com>
2021-11-02 16:01:52 -0700

    Changes? Yes.

--- thing.py
+++ thing.py
@@ -1,11 +1,10 @@
 #establishes libraries req'd
-import subprocess 
+import subprocess as sp
 import getpass as gp
 
-=======
 # @TheOriginalTimApple, what's this do? Also pick a better name than "run"
 def run(self, cmd):
-    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    completed = sp.run(["powershell", "-Command", cmd], capture_output=True)
     return completed
 
 def get_spot_prefs_path(username):
@@ -14,9 +13,10 @@ def get_spot_prefs_path(username):
 def get_last_version_file_path(username):
 	return "C:\\Users\\"+username+"\\AppData\\Roaming\\AutoSpotBlock\\LastVersion.dat"
 	# might have to create that directory before making the file or something
+    # >the directory may need to contain the drive name, not just "C:"
 
 # gets windows username
-username = getpass.getuser()
+username = gp.getuser()
 
 # opens prefs file in read-only
 PrefsPath = get_spot_prefs_path(username)
@@ -25,4 +25,4 @@ PrefsFile = open(PrefsPath,"r")
 # opens last version file in read+write
 LastVersionPath = get_last_version_file_path(username)
 LastVersionFile = open(LastVersionPath,"r+")
->>>>>>> 46bd7055e1ba36bd8f611026af1b494763f482ca
+
