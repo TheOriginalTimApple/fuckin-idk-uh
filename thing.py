4786fcb698591714853e6bc91132ad6a0a5afa24
TheOriginalTimApple <66660173+TheOriginalTimApple@users.noreply.github.com>
2021-11-02 09:45:26 -0700

    made comments cause thats all I know how to do

--- thing.py
+++ thing.py
@@ -2,5 +2,11 @@
 username = input("Idk how to find this on my own so what's your name lamo? >")
 path = "C:\\Users\\"+username+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
 
+# -get the version number
+
+# -compare to old value
+
+# -if theres a difference, do the thing
+
 # parse the thing and stuff
 # spotify_version = 
