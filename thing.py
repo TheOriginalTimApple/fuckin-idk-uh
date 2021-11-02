e52686a375c99e049e1b02f0d9c217831d41bbbe
TheOriginalTimApple <66660173+TheOriginalTimApple@users.noreply.github.com>
2021-11-02 12:25:15 -0700

    did a thint
    added a ps thing

--- thing.py
+++ thing.py
@@ -1,4 +1,6 @@
 #init shit <- bruh what's this mean
+Set-ExecutionPolicy RemoteSigned
+
 username = input("Idk how to find this on my own so what's your name lamo? >")
 path = "C:\\Users\\"+username+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
 
