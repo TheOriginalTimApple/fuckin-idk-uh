f7f9d43e09481a9c4bdc76197360b6fdb4bc135d
ARealSeal <53285975+arealseal@users.noreply.github.com>
2021-11-02 16:09:14 -0700

    fixed the bad

--- thing.py
+++ thing.py
@@ -1,10 +1,10 @@
 #establishes libraries req'd
-import subprocess as sp
-import getpass as gp
+import subprocess
+import getpass
 
 # @TheOriginalTimApple, what's this do? Also pick a better name than "run"
 def run(self, cmd):
-    completed = sp.run(["powershell", "-Command", cmd], capture_output=True)
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
     return completed
 
 def get_spot_prefs_path(username):
@@ -14,9 +14,10 @@ def get_last_version_file_path(username):
 	return "C:\\Users\\"+username+"\\AppData\\Roaming\\AutoSpotBlock\\LastVersion.dat"
 	# might have to create that directory before making the file or something
     # >the directory may need to contain the drive name, not just "C:"
+	# >>No
 
 # gets windows username
-username = gp.getuser()
+username = getpass.getuser()
 
 # opens prefs file in read-only
 PrefsPath = get_spot_prefs_path(username)
