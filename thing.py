6036a14f6d453afa44cbef5449061548121f6bd9
TheOriginalTimApple <66660173+TheOriginalTimApple@users.noreply.github.com>
2021-11-02 21:03:42 -0700

    more functional updates

--- thing.py
+++ thing.py
@@ -3,40 +3,43 @@ import subprocess
 import getpass
 import pathlib
 
-
-
 # @TheOriginalTimApple, what's this do? Also pick a better name than "run"
 #def run(self, cmd):
     #completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
     #eturn completed
 
-# Returns path to Spotify's prefs file
-
 def get_spot_prefs_path():
 	# Returns path to Spotify's prefs file
 	return pathlib.Path.home().joinpath("AppData","Roaming","Spotify","prefs")
+
 def get_last_version_file_path():
 	# Returns path to LastVersion.dat file.
 	return pathlib.Path.cwd().joinpath("LastVersion.dat")
 
 def get_spotify_version():
-	f = open('prefs', 'r')	
-	SpotifyVersionCurrent = f.readline()
+	# obtains current spotify version
+	f = open( get_spot_prefs_path() , 'r')	
+	SpotifyVersion = f.readline() 
+	f.close()	
+	return SpotifyVersion
+	
 
-# writes current spotify version to LastVersion.dat
 def update_current_version():
-	f = open('LastVersion.dat','w','-1')
-	f.write(str(SpotifyVersionCurrent))
+	# writes current spotify version to LastVersion.dat
+	f = open("LastVersion.dat", "w")
+	f.write(get_spotify_version())
 	f.close()
+
 def get_second_quote_index(stringToScan):
 	# Returns location of second quote
 	return int(stringToScan.find("\n"))-1 
+
 def get_first_quote_index(stringToScan):
 	# Returns location of first quote
 	return int(stringToScan.find('"'))
 
-# executes the install .bat file (thing2)
 def reinstall():
+	# executes the install .bat file (thing2)
 	subprocess.call(str(pathlib.Path.cwd().joinpath("thing2.bat")))
 
 # gets windows username
@@ -50,17 +53,14 @@ with open(PrefsPath,"r") as f:
 # opens last version file in read+write
 LastVersionPath = get_last_version_file_path()
 with open(LastVersionPath,"r+") as LastVersionFile:
+
 	#Do the stuff in here
 
 	#Parse, etc.
-
-
-
-#reinstall();
-
+	
 	print("this is a placeholder so vs code won't yell at me. (-:")
 
+# get_spot_prefs_path()
+# update_current_version()
+# print(get_spotify_version())
 
-update_current_version()
-print(SpotifyVersionCurrent)
-	print("this is a placeholder so vs code won't yell at me. (-:")
