0cf14348c83613bb3507c77498a803bab217fd0e
TheOriginalTimApple <66660173+TheOriginalTimApple@users.noreply.github.com>
2021-11-02 17:12:27 -0700

    added a batch file thinger

--- thing.py
+++ thing.py
@@ -2,19 +2,22 @@
 import subprocess
 import getpass
 
-# @TheOriginalTimApple, what's this do? Also pick a better name than "run"
-def run(self, cmd):
-    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
-    return completed
-
+# obtains current spotify version info
 def get_spot_prefs_path(username):
-	return "C:\\Users\\"+username+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
+	return "C:\\Users\\"+username+"\\AppData\\Roaming\\Spotify\\prefs"
 
+# obtains previous version info
 def get_last_version_file_path(username):
 	return "C:\\Users\\"+username+"\\AppData\\Roaming\\AutoSpotBlock\\LastVersion.dat"
 	# might have to create that directory before making the file or something
     # >the directory may need to contain the drive name, not just "C:"
 	# >>No
+	# >>>oh ok, it errored out when I ran it - it says the directory doesn't exist
+	# >>>>I think there needs to be a command to create that file...
+
+# executes the install .bat file (thing2)
+def reinstall():
+	subprocess.call([r'C:\Users\"+username+"\Documents\GitHub\fuckin-idk-uh'])
 
 # gets windows username
 username = getpass.getuser()
