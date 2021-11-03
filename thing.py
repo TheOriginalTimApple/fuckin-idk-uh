c3924286d49d70d2742d5db8f22b41d19389d7d0
ARealSeal <53285975+arealseal@users.noreply.github.com>
2021-11-02 17:55:50 -0700

    more good

--- thing.py
+++ thing.py
@@ -1,32 +1,29 @@
 #import modules
 import subprocess
 import getpass
+import pathlib
 
 # @TheOriginalTimApple, what's this do? Also pick a better name than "run"
 def run(self, cmd):
     completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
     return completed
 
-def get_spot_prefs_path(username):
-	return "C:\\Users\\"+username+"\\AppData\\Roaming\\Spotify\\prefs\\prefs"
+# Returns path to Spotify's prefs file
+def get_spot_prefs_path():
+	return pathlib.Path.home().joinpath("AppData","Roaming","Spotify","prefs","prefs")
 
-def get_last_version_file_path(username):
-	return "C:\\Users\\"+username+"\\AppData\\Roaming\\AutoSpotBlock\\LastVersion.dat"
-	# might have to create that directory before making the file or something
-    # >the directory may need to contain the drive name, not just "C:"
-	# >>No
-
-# gets windows username
-username = getpass.getuser()
+# Returns path to LastVersion.dat file.
+def get_last_version_file_path():
+	return pathlib.Path.cwd().joinpath("LastVersion.dat")
 
 # opens prefs file in read-only
-PrefsPath = get_spot_prefs_path(username)
+PrefsPath = get_spot_prefs_path())
 with open(PrefsPath,"r") as f:
 	PrefsFile = f.read
 print(f.closed) #Checks to make sure the file's closed and I'm not doing a dumb-dumb. Delete later.
 
 # opens last version file in read+write
-LastVersionPath = get_last_version_file_path(username)
+LastVersionPath = get_last_version_file_path()
 with open(LastVersionPath,"r+") as LastVersionFile:
 	#Do the stuff in here
 	#Parse, etc.
